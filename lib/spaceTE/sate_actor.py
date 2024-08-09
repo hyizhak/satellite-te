bugs in computation graph fixed

lib/spaceTE/sate_actor.py
@@ -109,11 +109,14 @@ class SaTEActor(nn.Module):
             logging.info(f'Loading spaceTE model from {mpath}')
             self.load_state_dict(torch.load(mpath, map_location=self.device))
 
-    def save_model(self):
+    def save_model(self, losses):
         """Save from model fname."""
         mpath = self.model_path(True)
         logging.info(f'Saving spaceTE model to {mpath}')
         torch.save(self.state_dict(), mpath)
+        with open(mpath.replace('.pt', '.losses'), 'w') as f:
+            for loss in losses:
+                f.write(f'{loss}\n')
 
     def model_path(self, create_dir=False):
         """Return full name of the ML model."""
@@ -203,14 +206,14 @@ class SaTEActor(nn.Module):
         # test mode
         if deterministic:
             distribution = None
-            raw_action = mean.detach()
+            raw_action = mean
             log_probability = None
         # train mode
         else:
             # use normal distribution for action
             distribution = Normal(mean, std)
             sample = distribution.rsample()
-            raw_action = sample.detach()
+            raw_action = sample
             log_probability = distribution.log_prob(raw_action).sum(axis=-1)
 
         return raw_action, log_probability

lib/spaceTE/sate_env.py
@@ -164,7 +164,7 @@ class SaTEEnv(object):
         self.G_dgl.ndata['in_traffic'] = self.in_traffic
         self.G_dgl.ndata['out_traffic'] = self.out_traffic    
         
-        # remove demands within nodes and 0 demands
+        # demands to be allocated
         tm = torch.FloatTensor(self.flow_values).flatten().to(self.device)
         tm = torch.repeat_interleave(tm, self.num_path)
         # obs = torch.concat([self.capacity, tm]).to(self.device)
@@ -215,6 +215,7 @@ class SaTEEnv(object):
         else:
             start_time = time.time()
             action = self.transform_raw_action(raw_action)
+            # action = raw_action.flatten() * self.obs['traffic']
             if self.obj.endswith('total_flow'):
                 # total flow require no constraint violation
                 obs = torch.concat([self.obs['capacity'], self.obs['traffic']]).to(self.device)
@@ -228,13 +229,19 @@ class SaTEEnv(object):
         self._next_obs()
         return reward, info
     
+    def get_label(self):
+        if self.supervised and self.mode == 'train':
+            return self.train_label[self.idx].to(self.device)
+        if self.supervised and self.mode == 'test':
+            return self.test_label[self.idx].to(self.device)
+    
     def compute_loss(self, raw_action):
         
         assert self.supervised and self.mode == 'train'
 
         action = self.transform_raw_action(raw_action)
 
-        labels = self.train_label[self.idx].to(self.device)
+        labels = self.get_label()
 
         # Adding a small value epsilon to avoid zeros in the labels
         epsilon = 1e-10
@@ -290,7 +297,7 @@ class SaTEEnv(object):
 
     def round_action(
             self, action, round_demand=True, round_capacity=True,
-            num_round_iter=2):
+            num_round_iter=20):
         """Return rounded action.
         Action can still violate constraints even after ADMM fine-tuning.
         This function rounds the action through cutting flow.
@@ -322,7 +329,8 @@ class SaTEEnv(object):
                 edge_flow = torch_scatter.scatter(
                     path_flow[self.p2e[0]], self.p2e[1], dim_size = self.num_edge_node)
                 # util of each edge
-                util = 1 + (edge_flow/capacity-1).relu()
+                util = 1 + (edge_flow/capacity - 1).relu()
+                # util = edge_flow/capacity
                 # propotionally cut path flow by max util
                 util = torch_scatter.scatter(
                     util[self.p2e[1]], self.p2e[0], reduce="max")
@@ -691,10 +699,10 @@ class SaTEEnv(object):
             G.add_nodes_from(range(params.graph_node_num))
             ## 1. Inter-satellite links
             for e in edge_list:
-                if (e[0] == e[1]) :
+                if (e[0] == e[1]):
                     continue
                 if random.random() < self.num_failure:
-                    G.add_edge(e[0], e[1], capacity=0)
+                    G.add_edge(e[0], e[1], capacity=0.0001)
                 else:
                     G.add_edge(e[0], e[1], capacity=params.isl_cap)
                 # G.add_edge(e[1], e[0], capacity=params.isl_cap)
@@ -704,13 +712,13 @@ class SaTEEnv(object):
                 G.add_edge(sat2user(i), i, capacity=params.uplink_cap)
                 # Downlink
                 G.add_edge(i, sat2user(i), capacity=params.downlink_cap)
-            ## 3. Inter ground station links
-            for i in range(params.GrdStationNum):
-                for j in range(params.GrdStationNum):
-                    if i == j:
-                        continue
-                    G.add_edge(i + params.Offset5, j + params.Offset5, capacity=0)
-                    G.add_edge(j + params.Offset5, i + params.Offset5, capacity=0)
+            # ## 3. Inter ground station links
+            # for i in range(params.GrdStationNum):
+            #     for j in range(params.GrdStationNum):
+            #         if i == j:
+            #             continue
+            #         G.add_edge(i + params.Offset5, j + params.Offset5, capacity=0)
+            #         G.add_edge(j + params.Offset5, i + params.Offset5, capacity=0)
             
             # print(G.number_of_nodes(), G.number_of_edges())
             # print(len(G))

lib/spaceTE/sate_model.py
@@ -49,6 +49,8 @@ class SaTE():
             num_sample: number of samples in COMA reward
         """
 
+        self.losses = []
+
         for epoch in range(num_epoch):
 
             self.env.reset('train')
@@ -60,6 +62,7 @@ class SaTE():
 
             for idx in loop_obj:
                 loss = 0
+                self.actor_optimizer.zero_grad()
                 for _ in idx:
                     torch.cuda.empty_cache()
 
@@ -78,7 +81,7 @@ class SaTE():
                             raw_action, num_sample=num_sample)
                         loss += -(log_probability*reward).mean()
 
-                self.actor_optimizer.zero_grad()
+                self.losses.append(loss.item()/len(idx))
                 loss.backward()
                 self.actor_optimizer.step()
                 # break
@@ -149,6 +152,9 @@ class SaTE():
                     pseudo_action = torch.ones(raw_action.shape).to(raw_action.device)
                     reward, info = self.env.step(
                         pseudo_action, num_admm_step=num_admm_step)
+                    # label = self.env.get_label()
+                    # reward, info = self.env.step(
+                    #     label, num_admm_step=num_admm_step)
                 else:
                     reward, info = self.env.step(
                         raw_action, num_admm_step=num_admm_step)
@@ -183,7 +189,7 @@ class SaTE():
             print(f'runtime: {runtime_avg:.4f}, obj: {obj_avg:.4f}')
 
     def save_model(self):
-        self.actor.save_model()
+        self.actor.save_model(self.losses)
 
     def load_model(self, quantized, compiled, model_path=None):
         self.actor.load_model(quantized, model_path)